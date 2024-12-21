24fb8756836bd8b38163c3b36c056303f1ea4421
Ray <74573089+RayHyper@users.noreply.github.com>
2024-12-21 15:53:31 -0500

    Changed proxy api name to api1

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@ export default defineConfig({
   server:{
     port:3000,
     proxy:{
-      '/api':{
+      '/api1':{
         target: "https://openapi.api.govee.com/router/api/v1/device/control",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api1/, ''),
       }
     }
 
