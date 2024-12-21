bc27638fe135434df1b2e67b561f1f6ab7eac3b3
Ray <74573089+RayHyper@users.noreply.github.com>
2024-12-21 18:27:01 -0500

    Fixed error in check and server.js standard errors

--- api/server.js
+++ api/server.js
@@ -1,54 +1,53 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
-import cors from "cors";
+import express from 'express'
+import axios from 'axios'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const port = 5000;
-app.use(cors());
-app.use(express.json());
+const app = express()
+const port = 5000
+app.use(cors())
+app.use(express.json())
 
-const base_url = "https://openapi.api.govee.com"
+const base_url = 'https://openapi.api.govee.com'
 
-app.post("/device/onoff", (req, res) => {
-  const data = req.body;
-
-  axios
-    .post(base_url + "/router/api/v1/device/control", data, {
-      headers: {
-        "Content-Type": "application/json",
-        "Govee-API-Key": process.env.VITE_KEY,
-      },
-    })
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).send("Internal Server Error");
-    });
-});
-
-app.get("/device/info", (req, res) => {
+app.post('/device/onoff', (req, res) => {
+  const data = req.body
 
   axios
-    .get(base_url + "/router/api/v1/user/devices",{
+    .post(base_url + '/router/api/v1/device/control', data, {
       headers: {
-        "Content-Type": "application/json",
-        "Govee-API-Key": process.env.VITE_KEY,
-      },
+        'Content-Type': 'application/json',
+        'Govee-API-Key': process.env.VITE_KEY
+      }
     })
     .then((response) => {
-      res.json(response.data);
+      res.json(response.data)
     })
     .catch((error) => {
-      console.log(error);
-      res.status(500).send("Internal Server Error");
-    });
-});
+      console.log(error)
+      res.status(500).send('Internal Server Error')
+    })
+})
+
+app.get('/device/info', (req, res) => {
+  axios
+    .get(base_url + '/router/api/v1/user/devices', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Govee-API-Key': process.env.VITE_KEY
+      }
+    })
+    .then((response) => {
+      res.json(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).send('Internal Server Error')
+    })
+})
 
 app.listen(port, () => {
-  console.log(`Backend is running on port ${port}`);
-});
+  console.log(`Backend is running on port ${port}`)
+})
