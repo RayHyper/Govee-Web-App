b2a8ed0d22b45dc155640369607eafab51e38b40
Ray <74573089+RayHyper@users.noreply.github.com>
2024-12-21 17:21:42 -0500

    Removed proxy for cors

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   server:{
     port:3000,
-    proxy:{
-      '/api1':{
-        target: "https://openapi.api.govee.com/router/api/v1/device/control",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api1/, ''),
-      }
-    }
-
   },
   plugins: [
     vue(),
