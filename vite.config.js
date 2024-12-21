34cf0bad4686ead34d5bdeb7f6654a3e985c6235
Ray <74573089+RayHyper@users.noreply.github.com>
2024-12-21 17:31:48 -0500

    Fixed linter js standard errors

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    port:3000,
+  server: {
+    port: 3000
   },
   plugins: [
     vue(),
-    vueDevTools(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
