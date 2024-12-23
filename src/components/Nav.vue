4a3e4073ac903d8033b33b6c6f21b98aa58828a3
Ray <74573089+RayHyper@users.noreply.github.com>
2024-12-23 16:44:58 -0500

    Made nav bar

---
+++ src/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <RouterLink to="/">
+    <button class="home">
+      <img
+        width="20"
+        height="20"
+        src="https://img.icons8.com/ios-filled/50/home.png"
+        alt="home"
+      />
+    </button>
+  </RouterLink>
+  <RouterLink to="/about">
+    <button class="info">
+      <img
+        width="20"
+        height="20"
+        src="https://img.icons8.com/ios/50/test--v1.png"
+        alt="test--v1"
+      />
+    </button>
+  </RouterLink>
+</template>
+
+<style scoped>
+.home,
+.info {
+  border-radius: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+}
+</style>
