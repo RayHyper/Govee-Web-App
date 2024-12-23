015ba07b45f570444b0de43ec41a1ac7aa6c5bb5
Ray <74573089+RayHyper@users.noreply.github.com>
2024-12-23 16:44:31 -0500

    Added vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from "vue";
 
+import { createRouter, createWebHistory } from "vue-router";
+
 import App from "./App.vue";
+import Main from "./components/Main.vue";
+import About from "./views/AboutView.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "MainApp", component: Main },
+    { path: "/about", name: "About", component: About },
+  ],
+});
 
 const app = createApp(App);
 
-app.mount("#app");
+app.use(router).mount("#app");
