8345467d81ea40220e126b6c5dd6c143c17089b5
Ray <74573089+RayHyper@users.noreply.github.com>
2024-12-23 16:45:05 -0500

    Made about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <h1>Extra</h1>
+
+  <hr />
+  <img
+    style="height: 200px;"
+    src="https://cdn.shopify.com/s/files/1/0560/4730/4877/products/H6052-1.jpg?v=1681293549"
+    alt=""
+  />
+
+  <p>
+    This is a Web application designed to control the Govee Lamp H6052. Users
+    can turn the lamp on and off and control other settings through a simple web
+    interface.
+  </p>
+
+  <div>
+    <div>
+      <h2>Credit:</h2>
+    </div>
+    <div>
+      <a
+        href="https://eu.govee.com/products/aura-table-lamp?srsltid=AfmBOoq0fS6vVAHB2zZiTLsXS6jHXD1Vkp729DMqaY3bP563hIdvjBXY"
+        >Click here</a
+      >
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+a {
+  text-decoration: none;
+  color: blue;
 }
 </style>
